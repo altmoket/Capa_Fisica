3f078f49ad307d10b66ee8d8dabe1414905401d6
copy90471 <copy90471@gmail.com>
2022-04-05 23:01:20 +0000

    Renaming system.py to network.py and adding config.txt file

--- main.py
+++ main.py
@@ -1,30 +1,16 @@
 from time import sleep
-from controllers.system import System
-system = System()
+from controllers.network import Network
 
-instructions = open("script.txt",'r')
+def main():
+    network =Network()
+    instructions = open("script.txt",'r')
+    for instruction in instructions:
+        network.execute(instruction)
+        instructions.close()
+    sleep(2)
+    network.isWorking = False
 
-for instruction in instructions:
-    system.execute(instruction)
-instructions.close()
 
-def print_devices_info(devices):
-    for device in devices:
-        print("Device: " + device.name)
-        if device.number_of_ports == 1:
-            port = device.getPorts() 
-            print(device.name + " -> " + port.name)
-            if port.cableConnected():
-                print(port.name + " ::connected with:: " + port.cable.next_port(port.name).name)
-        elif device.number_of_ports > 1:
-            ports = device.getPorts()
-            for port in ports:
-                print(device.name + " -> " + port.name)
-                if port.cableConnected():
-                    print(port.name + " ::connected with:: " + port.cable.next_port(port.name).name)
-                pass
-        pass
-sleep(2)
-devices = system.getDevices()
-print_devices_info(devices)
-system.isWorking = False
+
+
+main()
